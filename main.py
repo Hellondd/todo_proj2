940b9afb5472add73f409bec9e863daae78df9a1
Hellondd <skidman12@yandex.ru>
2026-04-13 10:59:34 +0300

    feat: реализована функция удаления задач

--- main.py
+++ main.py
@@ -8,7 +8,10 @@ def show_tasks():
     print("Список задач:")
     for i, t in enumerate(tasks, 1):
         print(f"{i}. {t}")
-
+def remove_task(index):
+    if 0 <= index < len(tasks):
+        removed = tasks.pop(index)
+        print(f"Задача '{removed}' удалена.")
 if __name__ == "__main__":
     add_task("Настроить Git")
     show_tasks()
